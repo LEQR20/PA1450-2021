21efe80b08af12f4e81ff057d8bb302c47eafbd4
Marius Stokkedal <matk20@student.bth.se>
2021-04-28 21:26:07 +0200

    Create countryList.py

---
+++ countryList.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+def home():
+    df=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+    print(df.head())
+    return(df)
+
+df = home()
+land = np.array([df.iloc[:,-1]])
+print(land)
+landnamn = df["Country/Region"].to_numpy()
+print(landnamn)
+
+dictonary = {}
+
+for i in land:
+    for j in landnamn:
+        dictonary[str(j)]=int(i)
+print(dictonary)
+"""
+print(df.iloc[:,-1])
+print("\n")
+test = df[df["Country/Region"]=="Afghanistan"]
+print(test.iloc[:,-1])
+b = int(test.iloc[:,-1])
+print(b, "b")
+print("\n")
+print(test)
+#print(df)
+"""
