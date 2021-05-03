1200ad909cf423a91b3413e7d1f8a01dd3485c8c
Marius Stokkedal <matk20@student.bth.se>
2021-05-03 11:49:39 +0200

    Added comments and descriptions

--- countryList.py
+++ countryList.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 
 def home():
+    """Downloads the csv-file of the data"""
     df=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
     return(df)
 
 def tableSorter(df):
-    index = list(df.columns)
-    countryList = df.iloc[:, [1,-1]]
-    sortedList = countryList.sort_values(index[-1], axis=0, ascending=False)
+    """Creates a new DF of the Country-column and the total cases column"""
+    index = list(df.columns) #Creates a list of all column-names for use as index
+    countryList = df.iloc[:, [1,-1]] #Creates a DF of the country- and total cases-column
+    sortedList = countryList.sort_values(index[-1], axis=0, ascending=False) #Sorts the new DF in descending order based on total cases
     return(sortedList)
 
 
